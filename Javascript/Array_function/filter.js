d4af52d31fe555e5ca62ca167ba5bddc996dabea
ji-park <qkrskdjf@naver.com>
2021-08-15 23:42:30 +0900

    유용한 배열함수 정리

--- Javascript/Array_function/filter.js
+++ Javascript/Array_function/filter.js
@@ -3,11 +3,41 @@ let result = numbers.filter((number) => {
     if (number > 5)
         return number;
 })
-console.log(result);
+// console.log(result);
 
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 result = words.filter(word => {
     if (word.length > 6)
         return word;
 })
+// console.log(result);
+
+var arr = [
+    { id: 15 },
+    { id: -1 },
+    { id: 0 },
+    { id: 3 },
+    { id: 12.2 },
+    { },
+    { id: null },
+    { id: NaN },
+    { id: 'undefined' }
+  ];
+
+function isNumber(obj) {
+    if (obj !== undefined && typeof(obj) === 'number' && !isNaN(obj))
+        return obj;
+}
+
+let count = 0;
+
+result = arr.filter(item =>{
+    if (isNumber(item.id) && item.id !== 0)
+    {
+        return item.id;
+    }
+    else
+        count++;
+})
+
 console.log(result);
